05c6781cc3380c504b15e666217f3dcbab46f095
Jun Zheng <me@jackzh.com>
2019-01-17 23:26:40 -0500

    fix: Everything should be good

--- Views/Student/quiz/main.js
+++ Views/Student/quiz/main.js
@@ -1,4 +1,9 @@
 import App from './App';
+import * as Sentry from '@sentry/browser';
+
+Sentry.init({
+    dsn: "https://8d454cf157e746c89ae93cc469d59953@sentry.io/1374235"
+});
 
 import React                       from 'react'
 import ReactDOM                    from 'react-dom';
