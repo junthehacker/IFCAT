14baffd7a7931bc18eed594e06ede46b9bb2e433
Jun Zheng <me@jackzh.com>
2019-01-22 16:29:26 -0500

    feature: Add support for  math$

--- Views/Student/quiz/contexts/GlobalContext.js
+++ Views/Student/quiz/contexts/GlobalContext.js
@@ -40,7 +40,12 @@ export function withGlobalContextProvider(WrappedComponent) {
             });
 
             // TODO: This is so hacky, have to have a better way
-            setTimeout(() => renderMathInElement(document.body), 100);
+            setTimeout(() => renderMathInElement(document.body, {
+                delimiters: [
+                    {left: "$$", right: "$$", display: true},
+                    {left: "$", right: "$", display: false}
+                ]
+            }), 100);
         };
 
         getData = () => {
